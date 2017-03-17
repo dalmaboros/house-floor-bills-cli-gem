4d21106be16f239ff8e97b93e0a4c9a9af5ba4d7
Dalma Boros <dalma.boros@gmail.com>
2017-03-17 13:23:38 -0500

    Stub cli.rb behaviors

--- lib/house_floor_bills/cli.rb
+++ lib/house_floor_bills/cli.rb
@@ -1,7 +1,16 @@
 class HouseFloorBills::CLI
-  
+
   def call
     puts "House Floor Bills"
+    list_bills
+  end
+
+  def list_bills
+    puts <<-DOC
+    1. H.R. 1029	Pesticide Registration Enhancement Act of 2017, as amended
+    2. H.R. 382   100 Years of Women in Congress Act
+    3. H.R. 1238	Securing our Agriculture and Food Act
+    DOC
   end
 
 end
