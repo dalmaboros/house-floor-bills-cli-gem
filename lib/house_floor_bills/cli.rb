125bd43776449cefbcb8b81da809969f3fe0fe40
Dalma Boros <dalma.boros@gmail.com>
2017-03-18 07:18:29 -0500

    Defined #menu in cli.rb

--- lib/house_floor_bills/cli.rb
+++ lib/house_floor_bills/cli.rb
@@ -1,16 +1,31 @@
 class HouseFloorBills::CLI
 
   def call
-    puts "House Floor Bills"
     list_bills
+    menu
   end
 
   def list_bills
-    puts <<-DOC
+    puts "House Floor Bills"
+    puts <<-DOC.gsub /^\s*/, ''
     1. H.R. 1029	Pesticide Registration Enhancement Act of 2017, as amended
     2. H.R. 382   100 Years of Women in Congress Act
     3. H.R. 1238	Securing our Agriculture and Food Act
     DOC
+  end
+
+  def menu
+    puts "Enter the number of the bill you want more info on"
+
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on bill 1..."
+      when "2"
+        puts "More info on bill 2..."
+      end
+    end
   end
 
 end
