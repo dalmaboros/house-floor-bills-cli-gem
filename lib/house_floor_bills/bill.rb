a43964fc6cab764cd780e81c35224360d62f9010
Dalma Boros <dalma.boros@gmail.com>
2017-03-19 22:05:02 -0500

    Started scraping logic

--- lib/house_floor_bills/bill.rb
+++ lib/house_floor_bills/bill.rb
@@ -1,6 +1,13 @@
 class HouseFloorBills::Bill
   attr_accessor :number, :name, :pdf, :url
 
+  def initialize(number = nil, name = nil, pdf = nil, url = nil)
+    @number = number
+    @name = name
+    @pdf = pdf
+    @url = url
+  end
+
   def self.this_week
     # Scrape House of Reps page & return bills based on that data
     self.scrape_bills
@@ -8,43 +15,23 @@ class HouseFloorBills::Bill
 
   def self.scrape_bills
     bills = []
-    # Go to page, find the bill
-    # Extract properties
-    # Instantiate a bill
-    bills
-
-    # bill_1 = self.new
-    # bill_1.number = "HR1029"
-    # bill_1.name = "Pesticide Registration Enhancement Act of 2017"
-    # bill_1.pdf = "http://docs.house.gov/billsthisweek/20170320/HR1029.pdf"
-    # bill_1.url = "https://www.congress.gov/bill/115th-congress/house-bill/1029"
-    #
-    # bill_2 = self.new
-    # bill_2.number = "HR382"
-    # bill_2.name = "100 Years of Women in Congress Act"
-    # bill_2.pdf = "http://docs.house.gov/billsthisweek/20170320/HR382.pdf"
-    # bill_2.url = "https://www.congress.gov/bill/115th-congress/house-bill/382"
-    #
-    # [bill_1, bill_2]
+    bills << self.scrape_site
   end
 
   def self.scrape_site
     doc = Nokogiri::HTML(open("http://docs.house.gov/floor/Default.aspx"))
-    # @number = doc.search(".legisNum").text
+    floor_items = doc.search("tr.floorItem")
+
+    a = floor_items.collect do |floor_item|
+      new(num = floor_item.css("td.legisNum").text,
+      floor_item.css("td.floorText").text.strip,
+      floor_item.css("td.files a").attr("href").text,
+      "https://www.congress.gov/bill/115th-congress/house-bill/#{num.split.last}")
+    end
 
     title = doc.search("div#primaryContent h1 > text()").text.strip.gsub("\r\n      ", " ")
 
-    bills = []
-
-    doc.css(".floorItem").each do |bill|
-      bills << {
-        :number => doc.css(".legisNum").text,
-        :name => doc.css(".floorText").text,
-        :pdf => doc.css(".files").text,
-        :url => "https://www.congress.gov/bill/115th-congress/house-bill/1029"
-      }
     end
-    bills
     puts title
     binding.pry
   end
