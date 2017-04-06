eedc7e1e9a1a648d0ad624c7a49df6bfa53f422e
Dalma Boros <dalma.boros@gmail.com>
2017-04-06 11:06:04 -0500

    Created #bills_by_status

--- lib/house_floor_bills/bill.rb
+++ lib/house_floor_bills/bill.rb
@@ -1,4 +1,20 @@
 class HouseFloorBills::Bill
   attr_accessor :number, :name, :pdf, :url, :sponsor, :committees, :status, :summary
 
+  @@bills = []
+
+  def initialize
+    @@bills << self
+  end
+
+  def self.all
+    @@bills.dup.freeze
+  end
+
+  def self.bills_by_status(status)
+    self.all.select do |bill|
+      bill.status.downcase == "#{status.downcase}"
+    end
+  end
+
 end
