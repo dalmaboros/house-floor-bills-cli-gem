dadc0daa2542e6898c13a220b14300d95d27b1f4
Dalma Boros <dalma.boros@geocent.com>
2020-05-15 13:14:05 -0500

    Remove redundant requirement

--- lib/house_floor_bills.rb
+++ lib/house_floor_bills.rb
@@ -5,7 +5,6 @@ require "nokogiri"
 require "pry"
 require "colorize"
 
-require_relative "./house_floor_bills/version"
 require_relative "./house_floor_bills/cli"
 require_relative "./house_floor_bills/bill"
 require_relative "./house_floor_bills/scraper"
